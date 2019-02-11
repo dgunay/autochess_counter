464008cd630d438ee67ad3ea40b9ffd83a9c2c73
dgunay <devingunay@gmail.com>
2019-02-10 20:26:26 -0800

    Add HeroCounter class

---
+++ WpfApp1/HeroCounter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoChess_Counter
+{
+    // Represents a hero. Constrains its count to 0 >= x <= 3
+    class HeroCounter
+    {
+        private int count;
+
+        public HeroCounter()
+        {
+            count = 0;
+        }
+
+        public void increment()
+        {
+            if (count < 3)
+            {
+                count++;
+            }
+        }
+
+        public void decrement()
+        {
+            if (count > 0)
+            {
+                count--;
+            }
+        }
+
+        public int getCount() { return count; }
+    }
+}
